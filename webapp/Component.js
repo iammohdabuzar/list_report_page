6b2325a9337801d78188fffb5a97cff0b1d7025a
Mohd Abuzar Ansari <abuzar.meerut@gmail.com>
2026-01-09 10:02:40 +0000

    Initial

---
+++ webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("practice.listreportpage.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
